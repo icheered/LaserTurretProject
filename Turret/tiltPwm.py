e12bd089bc7613a975381174df7c57b0f1447328
TwanBir <31514363+TwanBir@users.noreply.github.com>
2022-01-15 21:47:49 +0100

    Added some very nice comments, fixed bounding

--- Turret/tiltPwm.py
+++ Turret/tiltPwm.py
@@ -5,11 +5,14 @@ from machine import Pin
 class tiltMotor():
     def __init__(self, PWM_TILT_PIN):
         self.pin = PWM_TILT_PIN
+        # Init to standstill
         self.pwm = PWM(Pin(self.pin),freq=50,duty=77)
 
     def setTilt(self,tiltSpeed):
-        if (abs(tiltSpeed) > 25):
-            tiltSpeed = 25 if tiltSpeed > 0 else -25
+        # Set upper and lower bounds
+        tiltSpeed = 25 if tiltSpeed > 25 else tiltSpeed
+        tiltSpeed = -26 if tiltSpeed < -26 else tiltSpeed
+        # rewrite to correct period
         period = tiltSpeed + 77
         self.pwm.duty(period)
 
