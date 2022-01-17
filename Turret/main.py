975cf16a6153592b7236053151e78bbc5483ade9
Sebastiaan Hofstee <shofstee@sst-software.nl>
2022-01-17 16:59:51 +0100

    Initial commit outputting directions

--- Turret/main.py
+++ Turret/main.py
@@ -1,4 +1,33 @@
+from machine import Pin
 import uasyncio
+
+# First four pins next to ground on ESP8266
+pir0 = Pin(2, Pin.IN)
+pir1 = Pin(0, Pin.IN)
+pir2 = Pin(4, Pin.IN)
+pir3 = Pin(5, Pin.IN)
+pirPins = [pir0,pir1,pir2,pir3]
+
+motion = False
+
+def handle_interrupt_pir(pin):
+  global motion
+  motion = True
+  global interrupt_pin
+  interrupt_pin = pirPins.index(pin)
+
+
+pir0.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt_pir)
+pir1.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt_pir)
+pir2.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt_pir)
+pir3.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt_pir)
+
+async def detection():
+    global motion
+    while True:
+        if motion:
+            print(interrupt_pin)
+            motion = False
 
 async def blink(pin):
     while True:
@@ -9,11 +38,8 @@ async def blink(pin):
 
 async def main(pin):
     uasyncio.create_task(blink(pin))
+    uasyncio.create_task(detection())
     while True:
         await uasyncio.sleep(1)
-    
-
-from machine import Pin
-import time
 uasyncio.run(main(Pin(14, Pin.OUT)))
 print("Done!")
