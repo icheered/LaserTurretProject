42f9ef46040179a8365224169bda9120563d2de0
ICheered <tjeerd992@gmail.com>
2022-01-20 18:14:33 +0100

    Working version of transmission

--- Gun/src/main.py
+++ Gun/src/main.py
@@ -9,20 +9,29 @@ from ir_tx.nec import NEC
 
 
 def callback(data, addr, ctrl):
+    print("Callback is called")
     if data < 0:  # NEC protocol sends repeat codes.
         print("Repeat code.")
     else:
         print("Data {:02x} Addr {:04x}".format(data, addr))
 
 
-rx = NEC_8(Pin(32, Pin.IN), callback)
+receiver = NEC_8(machine.Pin(32, machine.Pin.IN), callback)
 
 transmitter = NEC(machine.Pin(12, machine.Pin.OUT))
 
 
-print("Starting")
+button = 0
+buttonPin = machine.Pin(26, machine.Pin.IN)
+
 i = 0
 while 1:
-    i += 1
-    transmitter.transmit(1, i)
-    time.sleep(1)
+    if buttonPin.value() and not button:
+        button = 1
+        print("Button went low")
+
+    elif not buttonPin.value() and button:
+        button = 0
+        print("Button went high" + str(i))
+        i += 1
+        transmitter.transmit(1, i)
