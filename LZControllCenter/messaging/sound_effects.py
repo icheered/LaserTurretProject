0b1bfb65db7ced6c03bd6e3e018cdd3b447997f9
s2234823 <b.j.hauser@student.utwente.nl>
2022-01-20 16:46:13 +0100

    sounds added

--- LZControllCenter/messaging/sound_effects.py
+++ LZControllCenter/messaging/sound_effects.py
@@ -11,40 +11,40 @@ import os
 def play_hit_sound():
     """Randomly select and play a sound from the list generated for when the
     turret is shot by a player."""
-    dir = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'Sounds', 'free', 'hit'))
-    sound_clip = AudioSegment.from_wav(dir + "/" + random.choice(os.listdir(dir)))
+    directory = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'Sounds', 'free', 'hit'))
+    sound_clip = AudioSegment.from_wav(directory + "/" + random.choice(os.listdir(directory)))
     play(sound_clip)
 
 
 def play_start_sound():
     """Randomly select and play a sound from the list generated for when the
     turret starts."""
-    dir = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'Sounds', 'free', 'start'))
-    sound_clip = AudioSegment.from_wav(dir + "/" + random.choice(os.listdir(dir)))
+    directory = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'Sounds', 'free', 'start'))
+    sound_clip = AudioSegment.from_wav(directory + "/" + random.choice(os.listdir(directory)))
     play(sound_clip)
 
 
 def play_target_detected_sound():
     """Randomly select and play a sound from the list generated for when
     a target is spotted."""
-    dir = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'Sounds', 'free', 'target found'))
-    sound_clip = AudioSegment.from_wav(dir + "/" + random.choice(os.listdir(dir)))
+    directory = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'Sounds', 'free', 'target found'))
+    sound_clip = AudioSegment.from_wav(directory + "/" + random.choice(os.listdir(directory)))
     play(sound_clip)
 
 
 def play_laser_fire_sound():
     """Randomly select and play a sound from the list generated for when the
     turret shoots at a player."""
-    dir = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'Sounds', 'free', 'fire'))
-    sound_clip = AudioSegment.from_wav(dir + "/" + random.choice(os.listdir(dir)))
+    directory = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'Sounds', 'free', 'fire'))
+    sound_clip = AudioSegment.from_wav(directory + "/" + random.choice(os.listdir(directory)))
     play(sound_clip)
 
 
 def play_target_lost_sound():
     """Randomly select and play a sound from the list generated for when the
     turret loses sight of the target."""
-    dir = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'Sounds', 'free', 'target lost'))
-    sound_clip = AudioSegment.from_wav(dir + "/" + random.choice(os.listdir(dir)))
+    directory = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'Sounds', 'free', 'target lost'))
+    sound_clip = AudioSegment.from_wav(directory + "/" + random.choice(os.listdir(directory)))
     play(sound_clip)
 
 
