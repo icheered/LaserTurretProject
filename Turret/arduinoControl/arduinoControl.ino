8feb76f1ca4de9a240c79dec350647d0a372a8b5
TwanBir <31514363+TwanBir@users.noreply.github.com>
2022-01-22 20:55:46 +0100

    Added angle control. Small bug that needs to be fix related to turret vibrating at certain set angles

--- Turret/arduinoControl/arduinoControl.ino
+++ Turret/arduinoControl/arduinoControl.ino
@@ -1,19 +1,24 @@
+#define PAN_SPEED 2
+#define PAN_ANGLE_REL 3
+#define PAN_ANGLE_ABS 4
+
+
 // SETTINGS
 int pulsePin = 11;
 int dirPin = 10;
-int maxSpeed = 170;
-int maxAcceleration = 17;
+int maxSpeed = 255;
+int maxAcceleration = 4;
 int maxAngle = 148;
 
 
 // random variables
 int incomingByte[3];
-int angle, speed, count; 
+int speed, count; 
 int countLim, nextCountLim;
 
 int nextOutput, nextDir, direction, nextSpeed;
 
-volatile short setValue;
+volatile short setValue, setAngle, angle;
 volatile byte opMode;
 
 void setup() {
@@ -24,7 +29,7 @@ void setup() {
   // 256 divisor
   TCCR2B = 0b00000110;
   // Sets 6.25Khz interrupt
-  OCR2A = 10;
+  OCR2A = 9;
   ASSR &= ~(1<<AS2);
   // enable interrupts
   TIMSK2 = 0b00000011;
@@ -45,8 +50,16 @@ void loop() {
     opMode = incomingByte[0];
 
     // retreive send value (16 bits)
-    setValue = (incomingByte[1]<<8) + (incomingByte[2]); 
-    Serial.println(setValue);
+    if (opMode == PAN_SPEED) {
+      setValue = (incomingByte[1]<<8) + (incomingByte[2]); 
+      Serial.println(setValue);
+    } else if (opMode == PAN_ANGLE_ABS) {
+      setAngle = (incomingByte[1]<<8) + (incomingByte[2]); 
+      Serial.println(setAngle);
+    } else if (opMode == PAN_ANGLE_REL) {
+      setAngle = angle + (incomingByte[1]<<8) + (incomingByte[2]); 
+      Serial.println(setAngle);
+    }
   }
 }
 
@@ -132,15 +145,22 @@ void speedControl() {
 }
 
 
-// Angle control implementation
+// Absolute ngle control implementation
 void angleControlAbs() {
-
-}
-
-
-// Angle control implementation relative
-void angleControlRel() {
-
+  int stepsLeft = setAngle - angle;
+  // check if allready at angle
+  if (stepsLeft == 0) {
+    stopToZero();
+  }
+  else if ((abs(speed) / maxAcceleration) >= abs(stepsLeft)) { // apparently not +1???
+    stopToZero();
+  }
+  else if (stepsLeft > 0) {
+    maxAccelerate(1);
+  }
+  else if (stepsLeft < 0) {
+    maxAccelerate(0);
+  }
 }
 
 
@@ -164,7 +184,7 @@ ISR(TIMER2_COMPA_vect) {
     nextOutput = ((speed == nextSpeed) && (nextSpeed == 0)) ? 0 : 1;
     if (!((speed == nextSpeed) && (nextSpeed == 0))) {
       angle = (nextDir == 1) ? angle + 1 : angle - 1;
-      Serial.println(nextSpeed);
+      //Serial.println(nextSpeed);
     }
     speed = nextSpeed;
     direction = nextDir;
@@ -176,11 +196,11 @@ ISR(TIMER2_COMPA_vect) {
   // evaluate if needing to break due to hitting limit
   if (approachingLimits()) {
     stopToZero();
-  } else if (opMode == 0) { // Speed control
+  } else if (opMode == PAN_SPEED) { // Speed control
     speedControl();
-  } else if (opMode == 1) { // angle control
-    angleControlRel();
-  } else if (opMode == 2) {
+  } else if (opMode == PAN_ANGLE_REL) { // angle control
+    angleControlAbs();
+  } else if (opMode == PAN_ANGLE_ABS) {
     angleControlAbs();
   }
 }
