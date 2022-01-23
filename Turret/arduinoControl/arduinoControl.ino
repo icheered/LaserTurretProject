765f214de5085ab650ed955c50c49c5a24a50877
TwanBir <31514363+TwanBir@users.noreply.github.com>
2022-01-23 19:05:02 +0100

    Total rework such that acceleration is linear instead of exponential, speed is between +/- 100 and no more bug, at the cost of my sanity

--- Turret/arduinoControl/arduinoControl.ino
+++ Turret/arduinoControl/arduinoControl.ino
@@ -1,25 +1,33 @@
+// operation codes
 #define PAN_SPEED 2
 #define PAN_ANGLE_REL 3
 #define PAN_ANGLE_ABS 4
+
+// speed settings
+#define COUNT_LIM_NUMERATOR 1000 // maxspeed * 5 for max ~180RPM
+#define ACCEL_CONSTANT 30
+#define MIN_SPEED 6
 
 
 // SETTINGS
 int pulsePin = 11;
 int dirPin = 10;
-int maxSpeed = 255;
-int maxAcceleration = 4;
+int maxSpeed = 100; // relatively arbitraty variable indicating max speed that can be set
+int maxAcceleration = 8;
 int maxAngle = 148;
 
 
 // random variables
 int incomingByte[3];
-int speed, count; 
-int countLim, nextCountLim;
+int speed, count, nextCountLim, stepsLeft, stepsNeededToStop;
+int countLim = 5;
+unsigned long pulseTime;
 
 int nextOutput, nextDir, direction, nextSpeed;
 
 volatile short setValue, setAngle, angle;
 volatile byte opMode;
+volatile bool breaking;
 
 void setup() {
   // put your setup code here, to run once:
@@ -46,6 +54,7 @@ void loop() {
     for (int i=0; i<3; i++) {
       incomingByte[i] = Serial.read();
     }
+    noInterrupts();
     // check operation mode (speed or absolute angle)
     opMode = incomingByte[0];
 
@@ -55,20 +64,28 @@ void loop() {
       Serial.println(setValue);
     } else if (opMode == PAN_ANGLE_ABS) {
       setAngle = (incomingByte[1]<<8) + (incomingByte[2]); 
+      breaking = false;
       Serial.println(setAngle);
     } else if (opMode == PAN_ANGLE_REL) {
+      breaking = false;
       setAngle = angle + (incomingByte[1]<<8) + (incomingByte[2]); 
       Serial.println(setAngle);
     }
+    interrupts();
   }
 }
 
+int stepsToStop() {
+  int calcValue = ((speed*speed) / 92);
+  return calcValue; // 2083/15
+}
+
 // determine if we should decelerate to avoid going over limit
-bool approachingLimits() {
+bool approachingLimits(byte operationMode) {
   // calculate steps left
-  int stepsLeft = maxAngle - abs(angle);
+  stepsLeft = maxAngle - abs(angle);
   // check if allready at max
-  if (stepsLeft <= 0) {
+  if (stepsLeft <= 0 && operationMode == PAN_SPEED) {
     if (angle > 0 && setValue < 0) {
       return false;
     } else if (angle < 0 && setValue > 0) {
@@ -77,7 +94,12 @@ bool approachingLimits() {
       //Serial.println(setValue);
       return true;
     }
-  } 
+  }
+  if (stepsLeft <= 0 && (operationMode == PAN_ANGLE_ABS || operationMode == PAN_ANGLE_REL)) {
+    if (abs(setAngle) < maxAngle) {
+      return false;
+    }
+  }
   // if stopped, return false
   if (speed == 0) {
     return false;
@@ -90,46 +112,57 @@ bool approachingLimits() {
     return false;
   }
   // see if we need to stop / slow down
-  if ((abs(speed) / maxAcceleration) >= stepsLeft) { // apparently not +1???
+  if (stepsToStop() >= stepsLeft) { // apparently not +1???
     return true;
   } 
 }
+
 
 
 // Maximum deceleration function
 // int 0 means ccw, 1 means cw
 void maxAccelerate(int rotDirection) {
   if (rotDirection == 1) {
-    nextSpeed = (abs(speed + maxAcceleration) <= maxSpeed) ? speed + maxAcceleration : maxSpeed;
+    nextSpeed = speed + (countLim / ACCEL_CONSTANT) + 1;
+    if (nextSpeed > maxSpeed) nextSpeed = maxSpeed;
+    if (abs(nextSpeed) < MIN_SPEED) nextSpeed = 0;
+    //nextSpeed = (abs(speed + maxAcceleration) <= maxSpeed) ? speed + maxAcceleration : maxSpeed;
   } else if (rotDirection == 0) {
-    nextSpeed = (abs(speed - maxAcceleration) <= maxSpeed) ? speed - maxAcceleration : -maxSpeed;
+    nextSpeed = speed - (countLim / ACCEL_CONSTANT) - 1;
+    if (nextSpeed < -maxSpeed) nextSpeed = -maxSpeed;
+    if (abs(nextSpeed) < MIN_SPEED) nextSpeed = 0;
+    //nextSpeed = (abs(speed - maxAcceleration) <= maxSpeed) ? speed - maxAcceleration : -maxSpeed;
   }
   nextDir = nextSpeed > 0 ? 1 : 0;
-  nextCountLim = nextSpeed != 0 ? 2550/abs(nextSpeed) : 2550/5;
+  nextCountLim = nextSpeed != 0 ? COUNT_LIM_NUMERATOR/abs(nextSpeed) : COUNT_LIM_NUMERATOR/MIN_SPEED;
 }
 
 void stopToZero() {
   if (direction == 1) {
-    nextSpeed = (speed - maxAcceleration) > 0 ? speed - maxAcceleration : 0;
+    nextSpeed = speed - (countLim / ACCEL_CONSTANT) - 1;
+    if (nextSpeed < MIN_SPEED) nextSpeed = 0;
+    //nextSpeed = (speed - maxAcceleration) > 10 ? speed - maxAcceleration : 0;
   } else if (direction == 0) {
-    nextSpeed = (speed + maxAcceleration) < 0 ? speed + maxAcceleration : 0;
+    nextSpeed = speed + (countLim / ACCEL_CONSTANT) + 1;
+    if (nextSpeed > -MIN_SPEED) nextSpeed = 0;
+    //nextSpeed = (speed + maxAcceleration) < -10 ? speed + maxAcceleration : 0;
   }
   nextDir = direction;
-  nextCountLim = nextSpeed != 0 ? 2550/abs(nextSpeed) : 2550/5;
+  nextCountLim = nextSpeed != 0 ? COUNT_LIM_NUMERATOR/abs(nextSpeed) : COUNT_LIM_NUMERATOR/MIN_SPEED;
 }
 
 void accelerateToValue(int accelValue) {
   nextSpeed = accelValue;
   nextDir = accelValue > 0 ? 1 : 0;
-  nextCountLim = 2550/(abs(nextSpeed));
+  nextCountLim = COUNT_LIM_NUMERATOR/(abs(nextSpeed));
 }
 
 
 //Speed control implementation 
 void speedControl() {
-  setValue = setValue <= 255 ? setValue : 255;
-  setValue = setValue >= -255 ? setValue : -255;
-  if (abs(setValue) < 5) {
+  setValue = setValue <= maxSpeed ? setValue : maxSpeed;
+  setValue = setValue >= -maxSpeed ? setValue : -maxSpeed;
+  if (abs(setValue) < MIN_SPEED) {
     stopToZero();
   }
   else if (abs(setValue - speed) > maxAcceleration) {
@@ -147,20 +180,24 @@ void speedControl() {
 
 // Absolute ngle control implementation
 void angleControlAbs() {
-  int stepsLeft = setAngle - angle;
+  stepsLeft = setAngle - angle;
+  stepsNeededToStop = stepsToStop();;
   // check if allready at angle
   if (stepsLeft == 0) {
+    nextSpeed = 0;
+    nextCountLim = COUNT_LIM_NUMERATOR/MIN_SPEED;
+  }
+  else if (stepsNeededToStop + 1 >= abs(stepsLeft)) {
+    breaking = true;
     stopToZero();
   }
-  else if ((abs(speed) / maxAcceleration) >= abs(stepsLeft)) { // apparently not +1???
-    stopToZero();
-  }
-  else if (stepsLeft > 0) {
+  else if (stepsLeft > 1 && breaking == false) {
     maxAccelerate(1);
   }
-  else if (stepsLeft < 0) {
+  else if (stepsLeft < 1 && breaking == false) {
     maxAccelerate(0);
   }
+
 }
 
 
@@ -170,6 +207,7 @@ void angleControlAbs() {
 
 // 6.25Khz timer interrupt
 ISR(TIMER2_COMPA_vect) {
+  noInterrupts();
   // increment iteration counter
   count++;
   // write current output values to pins
@@ -187,6 +225,7 @@ ISR(TIMER2_COMPA_vect) {
       //Serial.println(nextSpeed);
     }
     speed = nextSpeed;
+    pulseTime = millis();
     direction = nextDir;
     //Serial.println(speed);
   } else {
@@ -194,13 +233,14 @@ ISR(TIMER2_COMPA_vect) {
     nextOutput = 0;
   }
   // evaluate if needing to break due to hitting limit
-  if (approachingLimits()) {
+  if (approachingLimits(opMode)) {
     stopToZero();
   } else if (opMode == PAN_SPEED) { // Speed control
     speedControl();
   } else if (opMode == PAN_ANGLE_REL) { // angle control
     angleControlAbs();
-  } else if (opMode == PAN_ANGLE_ABS) {
+  } else if (opMode == PAN_ANGLE_ABS) {    
     angleControlAbs();
   }
+  interrupts();
 }
