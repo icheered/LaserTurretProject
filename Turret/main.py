1a0575d3f739e20aa6ce7bce22db359432e6ca97
TwanBir <31514363+TwanBir@users.noreply.github.com>
2022-01-25 00:12:05 +0100

    Added reading tiltPWM from the usb serial connection

--- Turret/main.py
+++ Turret/main.py
@@ -1,19 +1,26 @@
-import uasyncio
+import sys
+import select
+import tiltPwm
 
-async def blink(pin):
-    while True:
-        pin.on()
-        await uasyncio.sleep_ms(1)
-        pin.off()
-        await uasyncio.sleep_ms(1)
+PWM_TILT_PIN = 14
+TILT_PWM_OPCODE = 0
 
-async def main(pin):
-    uasyncio.create_task(blink(pin))
-    while True:
-        await uasyncio.sleep(1)
-    
+tiltMotor = tiltPwm.tiltMotor(PWM_TILT_PIN)
+tiltSpeed = 0
 
-from machine import Pin
-import time
-uasyncio.run(main(Pin(14, Pin.OUT)))
-print("Done!")
+p = select.poll()
+p.register(sys.stdin)
+message = bytearray()
+count = 0
+while True:
+	if (len(p.poll(0)) > 0):
+		message += sys.stdin.buffer.read(1)
+		count += 1
+	if (count > 2):
+		print(message)
+		if (message[0] == TILT_PWM_OPCODE):
+			tiltSpeed = message[2] + (message[1]<<8)
+			tiltMotor.setTilt(int(tiltSpeed/4))
+			print(tiltSpeed)
+		count = 0
+		message = bytearray()
