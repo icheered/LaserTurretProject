8e662af3a0f89f8c42d4bf4894f250cc31eb4c5b
Sebastiaan Hofstee <shofstee@sst-software.nl>
2022-01-24 11:13:52 +0100

    Cleaned up motion detection and added writing to UART

--- Turret/main.py
+++ Turret/main.py
@@ -1,33 +1,6 @@
 from machine import Pin
 import uasyncio
-
-# First four pins next to ground on ESP8266
-pir0 = Pin(2, Pin.IN)
-pir1 = Pin(0, Pin.IN)
-pir2 = Pin(4, Pin.IN)
-pir3 = Pin(5, Pin.IN)
-pirPins = [pir0,pir1,pir2,pir3]
-
-motion = False
-
-def handle_interrupt_pir(pin):
-  global motion
-  motion = True
-  global interrupt_pin
-  interrupt_pin = pirPins.index(pin)
-
-
-pir0.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt_pir)
-pir1.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt_pir)
-pir2.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt_pir)
-pir3.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt_pir)
-
-async def detection():
-    global motion
-    while True:
-        if motion:
-            print(interrupt_pin)
-            motion = False
+from motiondetection import MotionDetector
 
 async def blink(pin):
     while True:
@@ -38,8 +11,10 @@ async def blink(pin):
 
 async def main(pin):
     uasyncio.create_task(blink(pin))
-    uasyncio.create_task(detection())
+    uasyncio.create_task(detect.detection())
     while True:
         await uasyncio.sleep(1)
+
+detect = MotionDetector()
 uasyncio.run(main(Pin(14, Pin.OUT)))
 print("Done!")
---
+++ Turret/motiondetection.py
@@ -0,0 +1,39 @@
+from machine import UART
+from machine import Pin
+
+
+class MotionDetector:
+
+    def __init__(self):
+        # UART
+        self.uart = UART(1, 9600)
+        self.uart.init(9600, bits=8, parity=None, stop=1)
+
+        # Motion detection
+        self.interrupt_pin = -1
+        self.pirPins = []
+        self.motion = False
+        self.init_motion_detection()
+
+    def init_motion_detection(self):
+        # First four pins next to ground on ESP8266
+        pir0 = Pin(2, Pin.IN)
+        pir1 = Pin(0, Pin.IN)
+        pir2 = Pin(4, Pin.IN)
+        pir3 = Pin(5, Pin.IN)
+        pir0.irq(trigger=Pin.IRQ_RISING, handler=self.handle_interrupt_pir)
+        pir1.irq(trigger=Pin.IRQ_RISING, handler=self.handle_interrupt_pir)
+        pir2.irq(trigger=Pin.IRQ_RISING, handler=self.handle_interrupt_pir)
+        pir3.irq(trigger=Pin.IRQ_RISING, handler=self.handle_interrupt_pir)
+        self.pirPins = [pir0, pir1, pir2, pir3]
+
+    def handle_interrupt_pir(self, pin):
+        self.interrupt_pin = self.pirPins.index(pin)
+        self.motion = True
+
+    async def detection(self):
+        while True:
+            if self.motion:
+                frame = bytearray([6, self.interrupt_pin])
+                self.uart.write(frame)
+                self.motion = False
