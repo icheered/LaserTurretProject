aa5563dafc02f134fe347d13397eceaae08c96b0
TwanBir <31514363+TwanBir@users.noreply.github.com>
2022-01-25 14:59:19 +0100

    Fixed tilt input not reading correctly

--- Turret/main.py
+++ Turret/main.py
@@ -1,6 +1,7 @@
 import sys
 import select
 import tiltPwm
+from struct import unpack
 
 PWM_TILT_PIN = 14
 TILT_PWM_OPCODE = 0
@@ -10,17 +11,20 @@ tiltSpeed = 0
 
 p = select.poll()
 p.register(sys.stdin)
+opcode = bytearray()
 message = bytearray()
 count = 0
 while True:
 	if (len(p.poll(0)) > 0):
-		message += sys.stdin.buffer.read(1)
+		if count < 1:
+			opcode += sys.stdin.buffer.read(1)
+		else:
+			message += sys.stdin.buffer.read(1)
 		count += 1
 	if (count > 2):
 		print(message)
-		if (message[0] == TILT_PWM_OPCODE):
-			tiltSpeed = message[2] + (message[1]<<8)
-			tiltMotor.setTilt(int(tiltSpeed/4))
-			print(tiltSpeed)
+		if (opcode[0] == TILT_PWM_OPCODE):
+			tiltSpeed = unpack('>h', message)
+			tiltMotor.setTilt(int(tiltSpeed[0]/4))
 		count = 0
 		message = bytearray()
